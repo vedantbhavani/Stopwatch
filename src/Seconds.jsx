eb3bab789bcfde648c2986df9252454b3de22fcf
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-05-29 16:03:36 +0530

    change

--- src/Seconds.jsx
+++ src/Seconds.jsx
@@ -4,23 +4,35 @@ import './App.css'
 
 const Seconds = () => {
     const [IsRunning, setIsRunning] = useState(false)
-    const [time, settime] = useState(0)
+    const [second, setsecond] = useState(0)
+    const [minute, setminute] = useState(0)
+    const [milisec, setmilisec] = useState(0)
 
     let timer;
     useEffect(() => {
         if (IsRunning) {
             timer = setInterval(() => {
-                settime((previos) => previos + 1)
-            }, 1000);
+                setmilisec((milisec)=>milisec+1)
+                if(milisec===62){
+                    setsecond(second+1)
+                    setmilisec(0)
+                }
+                if(second===59){
+                    setminute(minute+1)
+                    setsecond(0)
+                }
+            }, 10);
         }
         else {
             clearInterval(timer)
         }
         return () => clearInterval(timer)
-    }, [IsRunning])
+    })
 
     const ReStartTime = () => {
-        settime(0)
+        setsecond(0)
+        setminute(0)
+        setmilisec(0)
     }
     const StartTime = () => {
         if (!IsRunning) {
@@ -37,7 +49,7 @@ const Seconds = () => {
     return (
         <>
             <div className="container">
-                <div className="timer">{time}</div>
+                <div className="timer">{minute<10?"0"+minute : minute} : {second<10 ? "0"+second :second}<span className='milisec'>:{milisec<10 ? "0"+milisec : milisec}</span></div>
                 <div className="startstop">
                 <button onClick={StartTime} disabled={IsRunning}>Start</button>
                 <button onClick={StopTime} disabled={!IsRunning}>Stop</button>
